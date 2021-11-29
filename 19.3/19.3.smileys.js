c6700c851b983265a6388e5a3634a2519ca4460e
yarentamir <yarden.tamir@walla.co.il>
2021-11-29 15:34:20 +0200

    msg alert to smileys solution

--- 19.3/19.3.smileys.js
+++ 19.3/19.3.smileys.js
@@ -1,12 +1,24 @@
 "use strict";
 
 const printSmileys = () => {
+  const inputValue = document.querySelector("#numberOfSmileys").value;
+  const container = document.querySelector(".container");
+
+  const msg = document.querySelector("#msg");
+  if (msg) container.removeChild(msg);
+
+  if (isNaN(inputValue)) {
+    let msgNotNum = document.createElement("div");
+    msgNotNum.textContent = "Not a number";
+    msgNotNum.id = "msg";
+    container.appendChild(msgNotNum);
+  }
+
   const showSmileyDiv = document.querySelector("#showSmileyDiv");
   if (showSmileyDiv) {
     showSmileyDiv.querySelectorAll("*").forEach((n) => n.remove());
   }
 
-  const inputValue = document.querySelector("#numberOfSmileys").value;
   for (let i = 1; i < parseInt(inputValue) + 1; i++) {
     const createImg = document.createElement("img");
     createImg.src =
