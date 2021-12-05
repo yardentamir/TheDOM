90b3a6e0ef649f546e17b90826da12890b1a5c37
yarentamir <yarden.tamir@walla.co.il>
2021-12-05 13:48:05 +0200

    another solution 233.2

--- 233.2-super_power.js
+++ 233.2-super_power.js
@@ -8,3 +8,14 @@ function printSuperPower() {
 }
 // printSuperPower.call(storm);
 storm.printSuperPower();
+
+const storm2 = {
+  superPower: "strong",
+  func: printSuperPower2,
+};
+
+function printSuperPower2() {
+  console.log("my superpower is " + this.superPower);
+}
+// printSuperPower.call(storm);
+storm2.func();
